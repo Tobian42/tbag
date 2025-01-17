32771585962e2c1fc705380a361033bb637150ee
tobian42 <github+project@tobian.dev>
2025-01-17 14:35:17 +0100

    modified:   .gitignore 	modified:   functions.py 	modified:   main.py 	modified:   requirements.txt

--- functions.py
+++ functions.py
@@ -1,14 +1,40 @@
-import sys
 import os
+import sys
 import time
 import cowsay
+import sshkeyboard
+
+msg_memory = []
 
 def sprint(chr, msg):
+   clear_console()
    str = cowsay.get_output_string(chr, msg)
    for c in str + '\n':
      sys.stdout.write(c)
      sys.stdout.flush()
      time.sleep(.005)
 
-def clear_console():
+def lprint(action, msg = None):
+   global msg_memory
+   if action == 'save':
+      msg_memory.append(msg)
+      return True
+   elif action == 'clear':
+      msg_memory = []
+      return True
+   elif action == 'load':
+      return("\n".join(msg_memory))
+   else:
+      return False
+
+def keyinput(target_key):
+    def press(key): pass
+    while sshkeyboard.listen_keyboard(on_press=press, until=target_key): pass
+
+def clear(scr = False):
    os.system('clear')
+   if 
+
+
+def inventory():
+   pass
--- main.py
+++ main.py
@@ -16,18 +16,28 @@ while True:
   if not tutorial:
     ans = input('Do you want to start the tutorial? [Y/n]\n>>>')
     if ans == 'N' or ans == 'n':
-      print('Skipping the tutorial..')
-      time.sleep(readtime)
-      tutorial = True
-      print('Tutorial skipped!')
-      time.sleep(readtime)
+        print('Skipping the tutorial..')
+        time.sleep(readtime)
+        tutorial = True
+        print('Tutorial skipped!')
+        time.sleep(readtime)
     else:
-      spint('tux', 'Welcome to the tutorial!\nI will be your guide through this game!')
-      time.sleep(readtime * 3)
-      
-    
-    sprint('tux', f'Hallo Welt: {hp, xp}')
-  hp -= 1
+        sprint('tux', 'Welcome to the tutorial!\nI will be your guide through this game!')
+        print('(Press "enter" to continue)')
+        keyinput("enter")
+        sprint('tux', 'Lets start with the Basics! Here is an apple, try to eat it.')
+        print('(Press "e" to open your inventory)')
+        keyinput("e")
+
+  
+
+
+  exit()
+
+
+
+
+
 
 clear_console()
 time.sleep(1)
